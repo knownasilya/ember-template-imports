94ea803227d712859078f61917c79e4c8288c06f
Ilya Radchenko <knownasilya@gmail.com>
2019-12-03 10:24:20 -0500

    fix: remove unused comments

--- index.js
+++ index.js
@@ -63,28 +63,6 @@ class TemplateImportProcessor extends BroccoliFilter {
       })
       .join("");
     let footer = imports.map(() => `{{/let}}`).join("");
-
-    // let header = imports.map(({ importPath, localName, isLocalNameValid }) => {
-    //   const warnPrefix = 'ember-template-component-import: ';
-    //   const abstractWarn = `${warnPrefix} Allowed import variable names - CamelCased strings, like: FooBar, TomDale`;
-    //   const componentWarn =  `
-    //     ${warnPrefix}Warning!
-    //     in file: "${relativePath}"
-    //     subject: "${localName}" is not allowed as Variable name for Template import.`;
-    //   const warn = isLocalNameValid ? '' : `
-    //     <pre data-test-name="${localName}">${componentWarn}</pre>
-    //     <pre data-test-global-warn="${localName}">${abstractWarn}</pre>
-    //   `;
-    //   if (!isLocalNameValid) {
-    //     this._console.log(componentWarn);
-    //     if (relativePath !== 'dummy/pods/application/template.hbs') {
-    //       // don't throw on 'dummy/pods/application/template.hbs' (test template)
-    //       throw new Error(componentWarn);
-    //     }
-    //   }
-    //   return `${warn}{{#let (component '${ importPath }') as |${ localName }|}}`;
-    // }).join('');
-    // let footer = imports.map(() => `{{/let}}`).join('');
     let result = header + rewrittenContents + footer;
     return result;
   }
